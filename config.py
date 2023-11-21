cbc3af004c24bba0331d0b30f1cdc3e12d72c627
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-11-21 22:14:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 class Config:
     API_ID = "20136640"
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    TOKEN = "6343560507:AAGZZbfeztF7JAPhDspnNp4YeCicKlvKLX0"
     MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
-    START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
-    SUDOERS = filters.user(["6691393517"])
+    START_PIC = "https://telegra.ph/file/230a240669f5a1fa1a762.jpg"
+    SUDOERS = filters.user(["5505030156"])
